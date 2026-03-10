f5b5b8894aeadbfbcc10a82e130dd21d711c5af9
Xursand <xursandgayratov397@gmail.com>
2026-03-10 17:42:40 +0500

    first commit

--- src/main/java/Kep/uz.java
+++ src/main/java/Kep/uz.java
@@ -1,22 +1,23 @@
 package main.java.Kep;
 
 public class uz {
-        public static void main(String[] args) {
-            String text = "Upper Case";
-            String text1 = "Upper Case";
-            String text2 = "Lower Case u uchun alohida method bor uchun";
-            String text3 = " Text ";
+    public static void main(String[] args) {
 
-            System.out.println(text.length());
-            System.out.println(text.toUpperCase());
-            System.out.println(text1.toLowerCase());
-            System.out.println(text2.indexOf("uchun"));
-            System.out.println(text.charAt(6));
-            System.out.println(text.equals(text1));
-            System.out.println("Dastlabki holat: "+"["+text3+"]");
-            System.out.println("Keyingi holat:   "+"["+text3.trim()+"]");
+        int[] array = {
+                13, 16, 21, 42, 11, 9,
+        };
+        int sum = 0;
+        for (int i = 1; i < array.length; i++) {
+            sum += array[i];
+            {
+
+            }
 
         }
+        System.out.println(sum/
+                array.length);
     }
+}
+
 
 
